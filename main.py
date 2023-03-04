e48b84e9b7bf8996ee3ffeff579889801fb8e24e
ahlam <ahlamahmed21101998@gmail.com>
2023-03-04 16:48:27 +0200

    finall commit

--- main.py
+++ main.py
@@ -7,11 +7,22 @@ import test_cases
 class TestSpeed (unittest.TestCase):
     def test_care_speed_low(self):
         self.assertEqual('Low', test_cases.car_speed(30))
-    def test_care_speed_normal(self):
-        self.assertEqual('Normal', test_cases.car_speed(100))
+        self.assertEqual('Low', test_cases.car_speed(3))
 
     def test_care_speed_normal(self):
+        self.assertEqual('Normal', test_cases.car_speed(100))
+        self.assertEqual('Normal', test_cases.car_speed(99))
+
+    def test_care_speed_invalid(self):
         self.assertEqual('Invalid', test_cases.car_speed(230))
+        self.assertEqual('Invalid', test_cases.car_speed(250))
+    def test_care_speed_v_high(self):
+        self.assertEqual('V.High', test_cases.car_speed(210))
+        self.assertEqual('V.High', test_cases.car_speed(215))
+    def test_care_speed_high(self):
+        self.assertEqual('High', test_cases.car_speed(150))
+        self.assertEqual('High', test_cases.car_speed(160))
+
 
 mysuit = unittest.TestSuite()
 mysuit.addTest(TestSpeed())
@@ -21,8 +32,12 @@ class TestScore (unittest.TestCase):
         self.assertEqual('Failed', test_cases.student_score(30))
     def test_care_speed_Passed(self):
         self.assertEqual('Passed', test_cases.student_score(60))
-    def test_care_speed_failed(self):
+    def test_care_speed_good(self):
         self.assertEqual('Good', test_cases.student_score(75))
+        self.assertEqual('Good', test_cases.student_score(80))
+    def test_care_speed_Excellent(self):
+        self.assertEqual('Excellent', test_cases.student_score(99))
+        self.assertEqual('Excellent', test_cases.student_score(86))
 
 
 mysuitscore = unittest.TestSuite()
